3f7f1e8c8d10958827afe7df1b095d05bf30b2e3
Moonzan <154397571+Moonzan@users.noreply.github.com>
2024-08-18 00:54:54 -0400

    Respawn from other point, deleted unused prefab

--- Assets/Scripts/Respawn.cs
+++ Assets/Scripts/Respawn.cs
@@ -1,27 +1,42 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Respawn : MonoBehaviour
 {
-    [SerializeField] private Transform respawnPoint; 
+    [SerializeField] private Transform[] respawnPoints;
     [SerializeField] private GameObject player;
+    private int respawnIndex = 0;
     // Delay time
     private float delayBeforeRespawn = 1f;
+    private HashSet<Collider2D> changedSpawnPoints = new HashSet<Collider2D>();
 
     void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Enemy")
         {
-            StartCoroutine(RespawnPlayerAfterDelay());
+            StartCoroutine(PlayerRespawn());
+            Debug.Log("Current respawn index: " + respawnIndex);
+        }
+        if (other.CompareTag("ChangeSpawnPoint") && !changedSpawnPoints.Contains(other))
+        {
+            // Increase respawn index with keeping the last respawn point
+            if (respawnIndex < respawnPoints.Length - 1)
+            {
+                respawnIndex++;
+            }
+            // Add changed spawn point to the list not to increase index again
+            changedSpawnPoints.Add(other); 
+            Debug.Log("Changed spawn point to index: " + respawnIndex);
         }
     }
 
-    IEnumerator RespawnPlayerAfterDelay()
+    IEnumerator PlayerRespawn()
     {
         // Wait enemy's movement finish
         yield return new WaitForSeconds(delayBeforeRespawn);
 
         // Move player to the respawn point
-        player.transform.position = respawnPoint.position;
+        player.transform.position = respawnPoints[respawnIndex].position;
     }
 }
