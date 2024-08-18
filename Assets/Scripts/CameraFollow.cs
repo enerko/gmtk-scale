a440a729f533350b0cf3e1b4fc7ecf836337c316
enerko <rene.koike@mail.utoronto.ca>
2024-08-17 20:55:09 -0400

    Added character sprite

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -7,7 +7,7 @@ public class CameraFollow : MonoBehaviour
     private float _yOffset = 1f;
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         Vector3 newPos = new Vector3(_target.position.x, _target.position.y + _yOffset, -10f);
         transform.position = Vector3.Slerp(transform.position, newPos, _followSpeed * Time.deltaTime);
