b286ff32a65b11f3d15e7f4d9d59adc0880a7a53
DystopiaGamez <152889056+DystopiaGamez@users.noreply.github.com>
2024-08-17 21:53:45 -0400

    UI final implimentation

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -1,19 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
+    private float _horizontal = 0;
+    private float _moveSpeed = 5f;
     private Rigidbody2D _rb;
-    private float _horizontal;
-    private float _vertical;
-
-    public float _moveSpeed = 5f;
-    public Vector2 boxSize = new Vector2(1f, 2f);
-    public float groundCastDistance = 0.1f;
-    public float wallCastDistance = 0.1f;
-    public LayerMask groundLayer;
-    public float _rotationSpeed = 10f;
-
-    private Vector3 _direction; // 1 if facing right, -1 if facing left
 
     void Start()
     {
@@ -22,111 +16,12 @@ public class Movement : MonoBehaviour
 
     void Update()
     {
-        // Get horizontal and vertical input
         _horizontal = Input.GetAxisRaw("Horizontal");
-        _vertical = Input.GetAxisRaw("Vertical");
 
-        if (IsNextToAWall() && (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.D)))
-        {
-            RotateToAlignWithWall();
-        }
-
-        if (!IsNextToAWall())
-        {
-            HandleMovement();
-        }
     }
 
-    private bool IsGrounded()
+    private void FixedUpdate()
     {
-        // Check if the player is grounded
-        RaycastHit2D hit = Physics2D.BoxCast(transform.position, boxSize, 0, -transform.up, groundCastDistance, groundLayer);
-        return hit.collider != null;
-    }
-
-    private bool IsNextToAWall()
-    {
-        // Check if the player is next to and facing a wall
-        Vector2 boxCastOrigin = transform.position + _direction * (boxSize.y / 2);
-        RaycastHit2D hit = Physics2D.BoxCast(boxCastOrigin, boxSize, 0, _direction, wallCastDistance, groundLayer);
-        return hit.collider != null;
-    }
-
-    private void RotateToAlignWithWall()
-    {
-        // Rotate the player
-        float angle = CalculateRotateAngle();
-
-        // Rotate the player
-        transform.Rotate(0, 0, angle);
-
-        // Rotate the _direction vector by the calculated angle
-        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-
-        // Apply the rotation to the _direction vector
-        _direction = rotation * _direction;
-    }
-
-    private void HandleMovement()
-    {
-        // If grounded, handle vertical movement as well
-        if (IsGrounded())
-        {
-            if (IsPlayerMovingVertical())
-            {
-                // Is player movement vertical
-                // If moving, check direction 
-                if (_vertical != 0) _direction.y = (int)(Mathf.Sign(_vertical));
-                _rb.velocity = new Vector2(0, _vertical * _moveSpeed);
-            }
-            else
-            {
-                // If moving, check direction 
-                if (_horizontal != 0) { _direction.x = (int)Mathf.Sign(_horizontal); }
-
-                _rb.velocity = new Vector2(_horizontal * _moveSpeed, 0);
-            }
-        }
-    }
-
-    private bool IsPlayerMovingVertical()
-    {
-        // Check that the vertical (W and D) movement is perpendicular to the wall
-        Vector2 verticalDirection = Vector2.up;
-        Vector2 playerDownDirection = -transform.up;
-        float dotProduct = Vector2.Dot(verticalDirection, playerDownDirection);
-
-        return Mathf.Abs(dotProduct) < 0.01f; // Using a small threshold for floating point precision
-    }
-
-    private float CalculateRotateAngle()
-    {
-        // Get the player's down direction and right direction
-        Vector2 downDir = -transform.up;
-
-        // Calculate the angle between downDirection and rightDirection
-        float angle = Vector2.Angle(downDir, _direction);
-
-        // clockwise or counterclockwise
-        float crossProduct = downDir.x * _direction.y - downDir.y * _direction.x;
-        if (crossProduct < 0)
-        {
-            angle = -angle;
-        }
-
-        return angle;
-    }
-
-    private void OnDrawGizmos()
-    {
-        // Wall detection box
-        Gizmos.color = Color.red;
-        Vector3 boxCenter = transform.position + _direction * (wallCastDistance / 2);
-        Gizmos.DrawWireCube(boxCenter, boxSize);
-
-        // Ground detection box
-        Gizmos.color = Color.blue;
-        Vector3 groundBoxCenter = transform.position - transform.up * (groundCastDistance / 2);
-        Gizmos.DrawWireCube(groundBoxCenter, boxSize);
+        _rb.velocity = new Vector2(_horizontal * _moveSpeed, _rb.velocity.y);
     }
 }
---
+++ Assets/Scripts/ScaleWheel.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleWheel : MonoBehaviour
+{
+    [SerializeField] private
+    int scaleAmount=0;
+    // Start is called before the first frame update
+    public void increaseScales()
+    {
+        scaleAmount+=1;
+        if(scaleAmount>1 &&scaleAmount<=6)
+        {
+            transform.GetChild(scaleAmount-1).gameObject.SetActive(false);
+        }
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(true);
+        }
+        if(scaleAmount<=6)
+        {
+            transform.GetChild(scaleAmount).gameObject.SetActive(true);
+        }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            increaseScales();
+        }
+    }
+}
